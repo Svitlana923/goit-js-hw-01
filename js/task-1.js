5285ea4a8758d84e22a71ea2161b5bf63b3aa670
Svitlana923 <svitlanakostiuk11@gmail.com>
2023-11-19 15:30:23 +0100

    1

---
+++ js/task-1.js
@@ -0,0 +1,8 @@
+"use strict";
+function makeTransaction(quantity, pricePerDroid) {
+console.log(makeTransaction = quantity * pricePerDroid);
+return (`You ordered ${quantity} droids worth ${makeTransaction} credits!`)
+};
+console.log(makeTransaction(5, 3000)); 
+console.log(makeTransaction(3, 1000)); 
+console.log(makeTransaction(10, 500)); 
